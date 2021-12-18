292a2aef5557e59100170102a5eec5ebe097d89d
20211tt2476Thuy <lethuy304202@gmail.com>
2021-12-18 16:46:05 +0700

    thuy add register and logout

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ class Logout {
 	public function __construct()
 	{
 		unset($_SESSION['user']); // xóa session user đã tạo khi đăng nhập
-		header('Location:login.php'); // chuyển hướng về trang chủ
+		header('location:login/index.php'); // chuyển hướng về trang chủ
 	}
 }
 $logout = new Logout();
--- models/user.php
+++ models/user.php
@@ -22,6 +22,27 @@ class User extends Db{
             return false;
         }
     }
-   
+    public function addUser($username, $password,$fullname,$address,$email,$phone)
+    {
+        $sql = self::$connection->prepare("INSERT INTO `users`(`username`, `password`, `role_id`, `fullname`, `address`, `email`, `phone`) VALUES (?,?,1,?,?,?,?)");
+        $password = md5($password);
+        $sql->bind_param("ssssss",$username, $password,$fullname,$address,$email,$phone);
+        $sql->execute();
+        return $sql;
+    }
+    public function checkUser($username)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM users WHERE `username`= ?");
+        $sql->bind_param("s",$username);
+        $sql->execute();
+        //$items = array();
+        $items = $sql->get_result()->num_rows;
+        if ($items == 1){
+           return true;
+       }
+       else {
+           return false;
+       }
+    }
 }
 
