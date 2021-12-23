fbf79ccf07ad184408144f13c024e658000b19c0
nguyenngoclananh <nguyenngoclananh.j7p@gmail.com>
2021-12-23 21:42:41 +0700

    lan anh update

--- addcart.php
+++ addcart.php
@@ -1,9 +1,29 @@
 <?php
 	include "header.php";
 ?>
-<h1><center> Your Cart</center></h1>
-
-
+<center>
+<h1> Your Cart</h1>
+<table border="1">
+    <tr>
+        <th colspan="3">Cart Information</th>
+    </tr>
+    <tr>
+        <td>Trần Anh Đức</td>
+        <td>03/08/1993</td>
+        <td>Nam</td>
+    </tr>
+    <tr>
+        <td>Kiều Thị Thu Hằng</td>
+        <td>04/09/1991</td>
+        <td>Nữ</td>
+    </tr>
+    <tr>
+        <td>Vương Thị Lê Na</td>
+        <td>06/10/1991</td>
+        <td>Nữ</td>
+    </tr>
+</table>
+</center>
 <?php
 
 include "footer.html";
